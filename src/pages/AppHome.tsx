f2ed6de22d2ffb694672a5c7632f028c43c9383f
‘webfer’ <mx7.bcn@pm.me>
2023-04-25 16:09:58 -0700

    feat: refactor function to show data on the podcast list

--- src/pages/AppHome.tsx
+++ src/pages/AppHome.tsx
@@ -1,33 +1,19 @@
 import { useEffect, useMemo, useState } from 'react';
 import ls from 'localstorage-slim';
 
-import { HeaderComponent, FilterPodcast } from '../components';
+import { HeaderComponent, FilterPodcast, Card } from '../components';
 import { Entry } from '../interfaces';
-import { Card } from '../components/Card';
 
-let BaseUrl =
+const BaseUrl =
   'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json';
 
+ls.config.encrypt = true;
+// Encrypt data on localstorage in order to protect data
 export const AppHome = () => {
   const [podcasts, setPodcasts] = useState<Entry[]>([]);
-
   const [filterPodTunes, setFilterPodTunes] = useState<string | null>(null);
 
-  useEffect(() => {
-    ls.get('PodTunes') === null
-      ? fetch(BaseUrl)
-          .then(async (response) => await response.json())
-          .then((response) => {
-            setPodcasts(response.feed.entry);
-            ls.set('PodTunes', response.feed.entry, { ttl: 30 });
-            console.log(response);
-          })
-          .catch((err) => {
-            console.log('Error fetching data: ', err);
-          })
-      : setPodcasts(ls.get('PodTunes') as React.SetStateAction<Entry[]>);
-  }, []);
-
+  // function to retrieve  by filter input and show data on the podcast lists
   const FilteredPodTunes = useMemo(() => {
     return typeof filterPodTunes === 'string' && filterPodTunes.length > 0
       ? podcasts.filter((podcasts) => {
@@ -43,6 +29,24 @@ export const AppHome = () => {
       : podcasts;
   }, [filterPodTunes, podcasts]);
 
+  useEffect(() => {
+    // validate if there is data on localStorage, if there isn't save it on localstorage
+    ls.get('PodTunes') === null
+      ? fetch(BaseUrl)
+          .then(async (response) => await response.json())
+          .then((response) => {
+            setPodcasts(response.feed.entry);
+            // save podcast in localstorage just for 1 day (86400)
+            ls.set('PodTunes', response.feed.entry, { ttl: 86400 });
+          })
+          .catch((err) => {
+            // catch error if it is generated
+            console.log('Error fetching data: ', err);
+          })
+      : // if there is data set podcast
+        setPodcasts(ls.get('PodTunes') as React.SetStateAction<Entry[]>);
+  }, []);
+
   return (
     <>
       <HeaderComponent />
@@ -50,8 +54,8 @@ export const AppHome = () => {
         FilteredPodTunes={FilteredPodTunes}
         setFilterPodTunes={setFilterPodTunes}
       />
-      {FilteredPodTunes.map((tunes, index) => (
-        <Card podcast={tunes} />
+      {FilteredPodTunes.map((pods, ind) => (
+        <Card podcast={pods} key={ind} />
       ))}
       <div>AppHome</div>
     </>
