6b02ecf9ae85a4aa05cdca6a7c56a02100781fff
‘webfer’ <mx7.bcn@pm.me>
2023-05-09 09:16:54 +0200

    feat: delete demo testing

--- __tests__/demo.test.tsx
+++
@@ -1,8 +0,0 @@
-test('prueba falla', () => {
-  // eslint-disable-next-line no-constant-condition
-
-  const mess1 = 'Hola mundo';
-
-  const mess2 = mess1.trim();
-  expect(mess1).toBe(mess2);
-});
